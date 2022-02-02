85062a357dfd078590242d6a40110d0c6515ef66
Tush1810 <tusharpruth@gmail.com>
2022-02-02 14:12:13 +0530

    Day 1 Ques 1

---
+++ Day 1/q1/README.md
@@ -0,0 +1,5 @@
+* https://codeforces.com/contest/1623/problem/C
+* Could not solve on my own.Had to check editorial
+* Good problem
+* level - 1600
+* Tags - Binary Search, Greedy
---
+++ Day 1/q1/q1.java
@@ -0,0 +1,170 @@
+import java.util.*;
+import java.lang.*;
+import java.math.BigInteger;
+
+import java.io.*;
+
+public class q1 implements Runnable {
+
+    public static void main(String[] args) {
+        new Thread(null, new q1(), "whatever", 1 << 26).start();
+    }
+
+
+    private FastScanner sc;
+    private PrintWriter pw;
+
+    public void run() {
+        try {
+            // pw = new PrintWriter(new BufferedWriter(new FileWriter("output.txt")));
+            // sc = new FastScanner(new BufferedReader(new FileReader("input.txt")));
+            pw = new PrintWriter(System.out);
+            sc = new FastScanner(new BufferedReader(new InputStreamReader(System.in)));
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+        int t = sc.nextInt();
+        // int t = 1;
+        while (t-- > 0) {
+            // sc.nextLine();
+            // System.out.println("for t=" + t);
+            solve();
+        }
+        pw.close();
+    }
+
+    public long mod = 1_000_000_007;
+
+    public void solve() {
+        int n = sc.nextInt();
+        int arr[] = sc.nextIntArray(n);
+        int temp[] = new int[n];
+        int max = arr[0], ans = arr[0];
+        for (int i = 1; i < n; i++) {
+            max = Math.max(arr[i], max);
+            ans = Math.min(arr[i], ans);
+            temp[i] = arr[i];
+        }
+        temp[0] = arr[0];
+        int start = 0, end = max;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            // System.out.println("start=" + start + " , end=" + end + " , mid=" + mid);
+            boolean found = false;
+            for (int i = 0; i < n; i++) {
+                arr[i] = temp[i];
+            }
+            for (int i = n - 1; i >= 2; i--) {
+                if (arr[i] < mid) {
+                    found = true;
+                    break;
+                }
+                int d = Math.min((arr[i] - mid), temp[i]) / 3;
+                arr[i - 1] += d;
+                arr[i - 2] += (2 * d);
+            }
+            if (found || arr[1] < mid || arr[0] < mid) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+                ans = Math.max(ans, mid);
+            }
+        }
+        pw.println(ans);
+    }
+
+
+    class FastScanner {
+        private BufferedReader reader = null;
+        private StringTokenizer tokenizer = null;
+
+        public FastScanner(BufferedReader bf) {
+            reader = bf;
+            tokenizer = null;
+        }
+
+        public String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public String nextLine() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    return reader.readLine();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken("\n");
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public float nextFloat() {
+            return Float.parseFloat(next());
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        }
+
+        public String[] nextStringArray(int n) {
+            String[] a = new String[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = next();
+            }
+            return a;
+        }
+
+        public long[] nextLongArray(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextLong();
+            }
+            return a;
+        }
+
+    }
+
+    public long fastPow(long x, long y, long mod) {
+        if (y == 0) return 1;
+        if (y == 1) return x % mod;
+
+        long temp = fastPow(x, y / 2, mod);
+        long ans = (temp * temp) % mod;
+
+        return (y % 2 == 1) ? (ans * (x % mod)) % mod : ans;
+    }
+
+    public long fastPow(long x, long y) {
+        if (y == 0) return 1;
+        if (y == 1) return x;
+
+        long temp = fastPow(x, y / 2);
+        long ans = (temp * temp);
+
+        return (y % 2 == 1) ? (ans * x) : ans;
+    }
+
+}
